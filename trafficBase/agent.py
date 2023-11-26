ed91a8bf798484f73bfbd31eadb77760b7033adf
pKYZ <rayojef@live.com.mx>
2023-11-26 13:56:35 -0600

    adding the smart movement of agents according to *star

--- trafficBase/agent.py
+++ trafficBase/agent.py
@@ -1,4 +1,5 @@
 from mesa import Agent
+from queue import PriorityQueue
 
 class Car(Agent):
     """
@@ -17,12 +18,15 @@ class Car(Agent):
         super().__init__(unique_id, model)
         self.name = "Car"
         self.availability = False
+        self.stopped = False
+        self.grid_Map = []
+        self.destination = []
 
     def move(self):
         """ 
         Determines if the agent can move in the direction that was chosen
         """
-        possible_steps = [x for x  in self.model.grid.get_neighborhood(self.pos, moore=True, include_center=True)]
+        """possible_steps = [x for x  in self.model.grid.get_neighborhood(self.pos, moore=True, include_center=True)]
         new_steps = []
         for step in possible_steps:
             try:
@@ -30,15 +34,118 @@ class Car(Agent):
                 for agent in agents:
                     if agent.availability == True:
                         new_steps.append(step)
+                    #if self.pos == step:
+                    #    self.stopped = False
             except:
                 pass
         #clean_steps = [a for a in possible_steps if a not in new_steps]
-        self.model.grid.move_agent(self, self.random.choice(new_steps))
+        self.model.grid.move_agent(self, self.random.choice(new_steps))"""
+        def manhattan_distance(a, b):
+            return abs(a[0] - b[0]) + abs(a[1] - b[1])
+        
+        def reconstruct_path(came_from, current):
+            path = []
+            while current in came_from:
+                current = came_from[current]
+                path.append(current)
+            return path
+        
+        def car_in_next_cell(direction):
+            Cars = []
+            for agent in self.model.grid.get_cell_list_contents(direction):
+                if agent.name == "Car":
+                    Cars.append(agent)
+            if Cars == []:
+                return False
+                
+        def green_light(position):
+            Traffic_Light = []
+            for agent in self.model.grid.get_cell_list_contents(position):
+                if agent.name == "Traffic Light":
+                    Traffic_Light.append(agent.state)
+            if Traffic_Light == []:
+                return True
+            elif Traffic_Light[0] == True:
+                return True
+
+        def aStar_path(grid, start, goal):
+            g_score = {cell:float('inf') for cell in grid}
+            f_score = {cell:float('inf') for cell in grid}
+            count = 0
+            came_from = {}
+            open_set = PriorityQueue()
+            open_set.put((0, count, start))
+            g_score[start] = 0
+            f_score[start] = manhattan_distance(start, goal)
+            grid_Map_keys = list(grid.keys())
+
+            open_set_hash = {start}
+
+            while not open_set.empty():
+                current = open_set.get()[2]
+                open_set_hash.remove(current)
+                if current == goal:
+                    path = []
+                    while current in came_from:
+                        path.append(current)
+                        current = came_from[current]
+                    path.append(start)
+                    path.reverse()
+                    return path
+
+                neighbors = []
+                for d in 'NSEW':
+                    if grid[current][d] == True:
+                        if d == 'N':
+                            neighbors.append((current[0], current[1] + 1))
+                            neighbors.append((current[0] + 1, current[1] + 1))
+                            neighbors.append((current[0] - 1, current[1] + 1))
+                        if d == 'S':
+                            neighbors.append((current[0], current[1] - 1))
+                            neighbors.append((current[0] + 1, current[1] - 1))
+                            neighbors.append((current[0] - 1, current[1] - 1))
+                        if d == 'E':
+                            neighbors.append((current[0] + 1, current[1]))
+                            neighbors.append((current[0] + 1, current[1] + 1))
+                            neighbors.append((current[0] + 1, current[1] - 1))
+                        if d == 'W':
+                            neighbors.append((current[0] - 1, current[1]))
+                            neighbors.append((current[0] - 1, current[1] + 1))
+                            neighbors.append((current[0] - 1, current[1] - 1))
+
+                clean_neighbors = [a for a in neighbors if a in grid_Map_keys]
+
+                for neighbor in clean_neighbors:
+                    temp_g_score = g_score[current] + 1
+                    if temp_g_score < g_score[neighbor]:
+                        came_from[neighbor] = current
+                        g_score[neighbor] = temp_g_score
+                        f_score[neighbor] = temp_g_score + manhattan_distance(neighbor, goal)
+                        if neighbor not in open_set_hash:
+                            count += 1
+                            open_set.put((f_score[neighbor], count, neighbor))
+                            open_set_hash.add(neighbor)
+
+        grid_tries = self.grid_Map
+        for tries in range(2):
+            next_move = aStar_path(grid_tries, self.pos, self.destination)
+            """if car_in_next_cell(next_move[1]) != False:
+                grid_tries.pop(next_move[1])
+                print(True if next_move[1] in grid_tries else False)"""
+            if car_in_next_cell(next_move[1]) == False:
+                if green_light(next_move[1]) == True:
+                    self.model.grid.move_agent(self, next_move[1])
+                    break
 
     def step(self):
         """ 
         Determines the new direction it will take, and then moves
         """
+        if self.pos == self.destination:
+            self.model.grid.remove_agent(self)
+            self.model.schedule.remove(self)
+            self.model.num_agents -= 1
+            return
         self.move()
 
 class Traffic_Light(Agent):
@@ -56,9 +163,29 @@ class Traffic_Light(Agent):
             timeToChange: After how many step should the traffic light change color 
         """
         self.state = state
-        self.timeToChange = timeToChange
+        self.last_state = "Green"
         self.name = "Traffic Light"
         self.availability = True
+        self.timeToChange = timeToChange
+    
+    def changeColor(self):
+        def is_cross_adjacent(a, b):
+            return a[0] == b[0] or a[1] == b[1]
+
+        radial_cells = [x for x in self.model.grid.get_neighbors(self.pos, moore=True, include_center=False, radius=3)]
+        neighboring_traffic_lights = []
+
+        for agent in radial_cells:
+            if agent.name == "Traffic Light" and not is_cross_adjacent(self.pos, agent.pos):
+                neighboring_traffic_lights.append(agent.state)
+
+
+        if self.state == False:
+            if neighboring_traffic_lights.count(True) <= 2:
+                self.state = not self.state
+        elif self.state == True:
+            if neighboring_traffic_lights.count(False) <= 2:
+                self.state = not self.state
 
     def step(self):
         """ 
@@ -104,7 +231,7 @@ class Road(Agent):
             direction: Direction where the cars can move
         """
         super().__init__(unique_id, model)
-        self.direction = direction
+        self.direction = {}
         self.name = "Road"
         self.availability = True
 
--- trafficBase/model.py
+++ trafficBase/model.py
@@ -2,7 +2,7 @@ from mesa import Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 from agent import *
-import json
+import json, random
 
 class CityModel(Model):
     """ 
@@ -17,7 +17,9 @@ class CityModel(Model):
         dataDictionary = json.load(open("city_files/mapDictionary.json"))
 
         self.traffic_lights = []
-
+        self.destinations = []
+        self.obstacles = []
+        self.grid_Map= {}
         # Load the map file. The map file is a text file where each character represents an agent.
         with open('city_files/2022_base.txt') as baseFile:
             lines = baseFile.readlines()
@@ -30,26 +32,54 @@ class CityModel(Model):
             # Goes through each character in the map file and creates the corresponding agent.
             for r, row in enumerate(lines):
                 for c, col in enumerate(row):
-                    if col in ["v", "^", ">", "<"]:
+                    if col in ["v", "^", ">", "<", "*"]:
                         agent = Road(f"r_{r*self.width+c}", self, dataDictionary[col])
+                        if col == "v":
+                            self.grid_Map[(c,self.height - r - 1)] = {'N': False, 'S': True, 'E': False, 'W': False}
+                        elif col == "^":
+                            self.grid_Map[(c,self.height - r - 1)] = {'N': True, 'S': False, 'E': False, 'W': False}
+                        elif col == "<":
+                            self.grid_Map[(c,self.height - r - 1)] = {'N': False, 'S': False, 'E': False, 'W': True}
+                        elif col == ">":
+                            self.grid_Map[(c,self.height - r - 1)] = {'N': False, 'S': False, 'E': True, 'W': False}
+                        elif col == "*":
+                            self.grid_Map[(c,self.height - r - 1)] = {'N': True, 'S': True, 'E': True, 'W': True}
                         self.grid.place_agent(agent, (c, self.height - r - 1))
 
-                    elif col in ["S", "s"]:
-                        agent = Traffic_Light(f"tl_{r*self.width+c}", self, False if col == "S" else True, int(dataDictionary[col]))
+                    elif col in ["◊", "∆", "«", "»"]:
+                        agent = Traffic_Light(f"tl_{r*self.width+c}", self, False if col in ["∆", "«"] else True, int(dataDictionary[col]))
+                        if col == "◊":
+                            self.grid_Map[(c,self.height - r - 1)] = {'N': False, 'S': True, 'E': False, 'W': False}
+                        elif col == "∆":
+                            self.grid_Map[(c,self.height - r - 1)] = {'N': True, 'S': False, 'E': False, 'W': False}
+                        elif col == "«":
+                            self.grid_Map[(c,self.height - r - 1)] = {'N': False, 'S': False, 'E': False, 'W': True}
+                        elif col == "»":
+                            self.grid_Map[(c,self.height - r - 1)] = {'N': False, 'S': False, 'E': True, 'W': False}
                         self.grid.place_agent(agent, (c, self.height - r - 1))
                         self.schedule.add(agent)
                         self.traffic_lights.append(agent)
 
                     elif col == "#":
                         agent = Obstacle(f"ob_{r*self.width+c}", self)
+                        position = [c, self.height - r - 1]
+                        self.obstacles.append(position)
                         self.grid.place_agent(agent, (c, self.height - r - 1))
 
                     elif col == "D":
                         agent = Destination(f"d_{r*self.width+c}", self)
+                        self.grid_Map[(c,self.height - r - 1)] = {'N': True, 'S': True, 'E': True, 'W': True}
+                        key = (c,self.height - r - 1)
+                        print('destination: ',key)
                         self.grid.place_agent(agent, (c, self.height - r - 1))
+                        self.schedule.add(agent)
+                        self.destinations.append((c, self.height - r - 1))
 
-        for i in range(5):
+
+        for i in range(N):
             agent = Car(f"c_{i}", self)
+            agent.grid_Map = self.grid_Map
+            agent.destination = random.choice(self.destinations)
             self.schedule.add(agent)
             self.grid.place_agent(agent, (i, 0))
 
